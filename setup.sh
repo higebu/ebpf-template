ed9ad93367ef768f38067b8cd2b0691b481e0a06
Yuya Kusakabe <yuya.kusakabe@gmail.com>
2019-01-19 22:19:03 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,9 +8,6 @@ rm -rf build
 mkdir build
 
 git clone --branch $KERNEL_VERSION --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git ./build/linux
-#sudo apt-get install -y linux-source-$KERNEL_VERSION
-#tar xf /usr/src/linux-source-$KERNEL_VERSION.tar.xz -C ./build
-#mv ./build/linux-source-$KERNEL_VERSION ./build/linux
 pushd ./build/linux
 make defconfig
 make headers_install
